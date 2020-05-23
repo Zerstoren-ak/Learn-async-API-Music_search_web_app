8c4c0cf4b47b64a496d73950e44f102094664652
Andrey <firebird_07@mail.ru>
2020-05-23 03:14:47 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,6 @@ slider.slick();
 //     console.log(slick)
 // });
 
-// const formatter = new Intl.DateTimeFormat(`ua`, {
-//     year: numeric,
-// });
-
 const formatter = new Intl.NumberFormat("en", {
     style: "currency",
     currency: "UAH",
@@ -24,7 +20,7 @@ const currentResult = document.getElementById(`currentResult`);
 const allResults = document.getElementById(`allResults`);
 let currentDate = new Date();
 
-if(!localStorage.userInfo) {
+if (!localStorage.userInfo) {
     localStorage.userInfo = JSON.stringify({});
 }
 const userInfo = JSON.parse(localStorage.userInfo);
@@ -32,46 +28,53 @@ const userInfo = JSON.parse(localStorage.userInfo);
 searchForm.addEventListener('submit', createSlider);
 videoSlider.addEventListener('click', videoManipulations);
 
-async function createSlider(event){
-        event.preventDefault();
-        const data = new FormData(this);
-        let response = await fetch(`https://itunes.apple.com/search?term=${data.get('query')}&entity=${data.get(`typeSelect`)}&limit=${data.get(`searchLimit`)}`);
-        let json = await response.json();
-        console.log(json);
-        const resultsArray = json.results;
-        console.log(resultsArray);
-        slider.slick('unslick');
-        slider.html(''); //maybe?
-        slider.slick({
-            slidesToShow: 1,
-            prevArrow: `<button class="btn-arrow btn-prev fas fa-chevron-left"></button>`,
-            nextArrow: `<button class="btn-arrow btn-next fas fa-chevron-right"></button>`,
-            draggable: false,
-        });
-        resultsArray.forEach(element => {
-            let template = createVideoTag(element);
-            slider.slick('slickAdd', template);
-        });
+async function createSlider(event) {
+    event.preventDefault();
+    const data = new FormData(this);
+    console.log(data.get(`typeSelect`));
+    let response = await fetch(`https://itunes.apple.com/search?term=${data.get('query')}&entity=${data.get(`typeSelect`) == `all` ? "" : data.get(`typeSelect`)}&limit=${data.get(`searchLimit`)}`);
+    let json = await response.json();
+    console.log(json);
+    const resultsArray = json.results;
+    console.log(resultsArray.length);
+    slider.slick('unslick');
+    slider.html(''); //maybe?
+    slider.slick({
+        slidesToShow: 1,
+        prevArrow: `<button class="btn-arrow btn-prev fas fa-chevron-left"></button>`,
+        nextArrow: `<button class="btn-arrow btn-next fas fa-chevron-right"></button>`,
+        draggable: false,
+    });
+    resultsArray.forEach(element => {
+        let template = createVideoTag(element);
+        slider.slick('slickAdd', template);
+    });
+
+    if(resultsArray.length != 0) {
         videoSlider.querySelector(`.slider-counter`).classList.remove(`hidden`);
-        await getCurrency();
+    } else {
+        videoSlider.querySelector(`.slider-counter`).classList.add(`hidden`);
+    }
+
+    await getCurrency();
 }
 
 async function getCurrency() {
     let findUSD = videoSlider.querySelectorAll(`.video-description`);
     let firstVisit = new Date(userInfo.date);
     let currency = 0;
-    if(currentDate.getDate() != firstVisit.getDate() && currentDate.getMonth() != firstVisit.getMonth() && currentDate.getFullYear() != firstVisit.getFullYear()) {
-        localStPutDate(userInfo,`userInfo`);
+    if (currentDate.getDate() != firstVisit.getDate() && currentDate.getMonth() != firstVisit.getMonth() && currentDate.getFullYear() != firstVisit.getFullYear()) {
+        localStPutDate(userInfo, `userInfo`);
         try {
             let response = await fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`);
             let currencyList = await response.json();
 
-            currencyList.forEach(element => {
+            currencyList.forEach((element) => {
                 if (element.ccy == `USD`) {
                     currency = Number(element.sale);
                 }
             });
-            findUSD.forEach(e => {
+            findUSD.forEach((e) => {
                 e.dataset.newprice = `${formatter.format(e.dataset.price * currency)}`;
             });
 
@@ -84,7 +87,7 @@ async function getCurrency() {
     } else {
         currency = userInfo.exchangeRate;
 
-        findUSD.forEach(e => {
+        findUSD.forEach((e) => {
             e.dataset.newprice = `${formatter.format(e.dataset.price * currency)}`;
         });
     }
@@ -109,42 +112,42 @@ function localStInput(ls_name, key, value) {
 // }
 
 function videoManipulations(e) {
-        const arrows = videoSlider.querySelectorAll(`.btn-arrow`);
-        if (e.target.classList.contains('video-button')) {
-            const video = e.target.previousElementSibling;
-            e.target.classList.add('hide');
-            video.play();
-            video.controls = true;
+    const arrows = videoSlider.querySelectorAll(`.btn-arrow`);
+    if (e.target.classList.contains('video-button')) {
+        const video = e.target.previousElementSibling;
+        e.target.classList.add('hide');
+        video.play();
+        video.controls = true;
 
-            arrows.forEach(e => {
-                e.classList.add(`color-dark`);
-            })
-        }
+        arrows.forEach(e => {
+            e.classList.add(`color-dark`);
+        })
+    }
 
-        if (e.target.tagName == `VIDEO`) {
-            e.target.controls = false;
-            e.target.pause();
+    if (e.target.tagName == `VIDEO`) {
+        e.target.controls = false;
+        e.target.pause();
+        e.target.nextElementSibling.classList.remove(`hide`);
+
+        arrows.forEach(e => {
+            e.classList.remove(`color-dark`);
+        });
+
+        e.target.addEventListener(`ended`, function () {
+            e.target.load();
             e.target.nextElementSibling.classList.remove(`hide`);
+            e.target.controls = false;
+        })
+    }
 
-            arrows.forEach(e => {
-                e.classList.remove(`color-dark`);
-            });
-
-            e.target.addEventListener(`ended`, function () {
-                e.target.load();
-                e.target.nextElementSibling.classList.remove(`hide`);
-                e.target.controls = false;
-            })
-        }
-
-        if (e.target.classList.contains('btn-arrow')) {
-            arrows.forEach(e => {
-                e.classList.remove(`color-dark`);
-            });
-        }
+    if (e.target.classList.contains('btn-arrow')) {
+        arrows.forEach(e => {
+            e.classList.remove(`color-dark`);
+        });
+    }
 }
 
-slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
     const videos = document.querySelectorAll('.slider video');
     videos.forEach((e) => {
         e.pause();
@@ -155,12 +158,13 @@ slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
     currentResult.textContent = nextSlide + 1;
 });
 
-  slider.on('destroy', function(e,slick){});
+slider.on('destroy', function (e, slick) {
+});
 
-  slider.on('reInit', function(event, slick, currentSlide, nextSlide){
-      currentResult.textContent = slick.currentSlide + 1;
-      allResults.textContent = slick.slideCount;
-  });
+slider.on('reInit', function (event, slick, currentSlide, nextSlide) {
+    currentResult.textContent = slick.currentSlide + 1;
+    allResults.textContent = slick.slideCount;
+});
 
 function createVideoTag(obj) {
     let releaseDate = new Date(obj.releaseDate);
